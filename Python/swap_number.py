afa08a7d47ee06065878e2ab94c03d1d95a3caa5
Vikasjadav <vikassen472@gmail.com>
2026-09-14 21:51:09 +0530

    swap_number

---
+++ Python/swap_number.py
@@ -0,0 +1,31 @@
+'''
+a=int(input("enter a :"))
+b=int(input("enter b :"))
+
+c=a
+a=b
+b=c
+
+print(f"a is {a} and b is {b}")
+'''
+'''
+a=int(input("enter a :"))
+b=int(input("enter b :"))
+a=a+b
+b=a-b
+a=a-b
+print(f"a is {a} and b is {b}")
+
+'''
+'''
+a=int(input("enter a :"))
+b=int(input("enter b :"))
+a=a^b
+b=a^b
+a=a^b
+print(f"a is {a} and b is {b}")
+'''
+a=int(input("enter a :"))
+b=int(input("enter b :"))
+a,b=b,a
+print(f"a is {a} and b is {b}")
