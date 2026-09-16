5e82f75134323c5a69e2ae3af66ad19434bbef19
Vikasjadav <vikassen472@gmail.com>
2026-09-16 14:29:51 +0530

    Q5

---
+++ Assignment1/Q5.py
@@ -0,0 +1,14 @@
+cost=1600
+rate=25
+length=20
+Perimeter=cost/rate
+
+
+breadth =(Perimeter/2) - length
+
+area = length * breadth
+
+print(f"permiter of the field : {Perimeter}")
+print(f"breadth of the field : {breadth}")
+print(f"area of the field : {area} ")
+
